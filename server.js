f8847db4177f3a637d2416fa8cef9f9015ef337c
Restituo <rcc999@comcast.net>
2019-01-09 15:38:11 -0800

    package changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-
+var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -10,8 +10,10 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("app"));
+//app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static("app"));
+//app.use(express.static("public"));
+//app.use(express.static("assets"));
 
 // Defines Routes to different JS files
 
